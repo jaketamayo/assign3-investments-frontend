b72772b9f7e812485b07e8ca4f9a719efe6c7d2c
Jake Tamayo <dead1nternet@proton.me>
2024-10-22 17:38:42 -0500

    First push

---
+++ dist/js/about.d34a3e82.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkinvestments_fe"]=self["webpackChunkinvestments_fe"]||[]).push([[594],{603:function(n,e,t){t.r(e),t.d(e,{default:function(){return f}});var s=t(6768);const u={class:"about"};function a(n,e){return(0,s.uX)(),(0,s.CE)("div",u,e[0]||(e[0]=[(0,s.Lk)("h1",null,"This is an about page",-1)]))}var r=t(1241);const c={},i=(0,r.A)(c,[["render",a]]);var f=i}}]);
+//# sourceMappingURL=about.d34a3e82.js.map
